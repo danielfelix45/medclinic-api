81775806af15b651e6a19ec89816357968a44979
Daniel Felix <felixdaniel-developer@outlook.com>
2026-09-08 20:45:36 -0300

    feat: configura porta da API por variavel de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
