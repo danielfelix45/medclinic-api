5be9c69a228abdeace60835f1d52a022305a8e52
Daniel Felix <felixdaniel-developer@outlook.com>
2026-09-10 21:38:42 -0300

    feat: registra rotas de autenticacao no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import { AppDataSource } from "./database/data-source";
+import { authRoutes } from "./routes/auth.routes";
 
 const app = express();
+app.use(express.json());
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
